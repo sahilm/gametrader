6e1a8e832bb0124bdda2991ee80fc28a6987c60e
Sahil Muthoo <sahil.muthoo@gmail.com>
2013-01-16 22:41:14 +0530

    * bundle update * get rid of unused fixture path in spec_helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,7 +20,6 @@ Spork.prefork do
   Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
   RSpec.configure do |config|
-    config.fixture_path = "#{::Rails.root}/spec/fixtures"
     config.use_transactional_fixtures = true
     config.infer_base_class_for_anonymous_controllers = false
     config.order = "random"
