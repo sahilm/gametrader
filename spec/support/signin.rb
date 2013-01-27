b0c1b29d8c937446a27ce9edf4a70a7c9ae7c960
Sahil Muthoo <sahil.muthoo@gmail.com>
2013-01-27 08:08:42 +0530

    bundle update

--- spec/support/signin.rb
+++ spec/support/signin.rb
@@ -1,10 +1,10 @@
 module Signin
   def assert_signed_in(page, user)
     page.should_not have_link 'Sign in'
-    page.should have_success_message("Welcome, #{user[:first_name]}!")
     page.should have_content user[:first_name]
     page.should have_selector ".icon-cog"
     page.should have_selector ".avatar"
+    page.should have_success_message("Welcome, #{user[:first_name]}!")
     page.click_link user[:first_name]
     page.should have_link "Sign out"
   end
